ed0c9a482059e870307f157efaa3867fc0226b24
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-04-29 21:51:37 -0300

    métodos filter e find - exemplo filtrar pedidos

---
+++ filterPedidos.js
@@ -0,0 +1,27 @@
+//Lista de Pedidos
+let pedidos = [
+    {'id': 420, 'nome':'Dener', 'alimento': 'Sandubão de Bacon', 'bebida': 'Suco Limão' },
+    {'id': 152, 'nome':'Naiady', 'alimento': 'Sandubão Vegano', 'bebida': 'Suco de Laranja' },
+    {'id': 29, 'nome':'Marcio', 'alimento': 'Coxinha', 'bebida': 'Suco de Uva' },
+    {'id': 33, 'nome':'Isabel', 'alimento': 'Sandubão de Picanha', 'bebida': 'Refrigerante' },
+    {'id': 55, 'nome':'José', 'alimento': 'Pizza', 'bebida': 'Refrigerante' }
+];
+
+//Iterador filter
+let meusPedidos = pedidos.filter( ( pedido ) => {
+    return pedido.bebida == 'Refrigerante'
+});
+
+console.log(meusPedidos)
+
+// [
+//     {
+//       id: 33,
+//       nome: 'Isabel',
+//       alimento: 'Sandubão de Picanha',
+//       bebida: 'Refrigerante'
+//     },
+//     { id: 55, nome: 'José', alimento: 'Pizza', bebida: 'Refrigerante' }
+//   ]
+
+// É interessante fazer um buscador com JS, utilizando o angular, ou rect, ou voo 
